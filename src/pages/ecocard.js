e564c564a075217b3229df7af94299f2afdd7ccc
ninaristicc <ninaristic@hotmail.com>
2019-07-22 18:41:47 -0700

    commit

--- src/pages/ecocard.js
+++ src/pages/ecocard.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Image from "../components/Image"
+import Image from "../components/image"
 
 const SecondPage = () => (
   <Layout>
