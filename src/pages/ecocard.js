82f3139320dda09f70ca61d41d5341c6692efd1b
ninaristicc <ninaristic@hotmail.com>
2019-07-22 18:04:24 -0700

    layout and style

--- src/pages/ecocard.js
+++ src/pages/ecocard.js
@@ -3,13 +3,22 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Image from "../components/Image"
 
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
+    <div class="ecocontent">
     <h1 class="main-h">Hi from the EcoCard page</h1>
     <p>Welcome</p>
-    <Link to="/">Go back to the homepage</Link>
+    <Link to="/">Go back to the homepage</Link> </div>
+    <aside class="sideBar">
+      <h2 class="side-h">Latest News</h2>
+      <article>
+      <div class="sideImg"><Image /></div>
+      <a href="/" class="side-a">Is Bitcoin The Future Of Online Paymants?</a>
+      </article>
+    </aside>
   </Layout>
 )
 
