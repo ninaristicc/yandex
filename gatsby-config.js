276f240a24f27be5eb2f6bb9ba7b1e4f5b54c17f
ninaristicc <ninaristic@hotmail.com>
2019-07-25 15:40:30 -0700

    canonical link

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,13 +27,7 @@ module.exports = {
         icon: `src/images/yandex.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-plugin-canonical-urls`,
-      options: {
-        siteUrl: `https://yandex-demo.netlify.com/`,
-        stripQueryString: true,
-      },
-    },
+  
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
